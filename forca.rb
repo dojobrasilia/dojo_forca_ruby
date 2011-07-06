4a830ff7cb9d6a1aa94e16bf0022d423029fdbe3
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2011-07-06 18:44:55 -0300

    so subtrai se errar o chute

--- forca.rb
+++ forca.rb
@@ -1,11 +1,20 @@
 class Forca
 	def initialize(palavra)
 		@palavra = palavra
+		@chutes= 10
 	end
 	
 	def output
 		out = ""
 		@palavra.size.times { out << '.' }
 		return out
+	end
+	
+	def chutes
+		return @chutes
+	end
+	
+	def chutar(char)
+		@chutes -= 1 unless @palavra.include?(char)
 	end
 end
--- forca_spec.rb
+++ forca_spec.rb
@@ -1,6 +1,6 @@
 require 'forca'
 
-describe 'init' do
+describe Forca do
 
 	it 'deve imprimir pontinhos para uma palavra' do
 		f = Forca.new('abacate')
@@ -10,5 +10,23 @@ describe 'init' do
 	it 'deve imprimir pontinhos para uma palavra diferente' do
 		f = Forca.new('maca')
 		f.output.should == '....'
+	end
+	
+	it 'inicia com 10 chutes' do
+		f= Forca.new('oi')
+		f.chutes.should == 10
+	end
+	
+	it 'perde um chute qdo chuta errado' do
+		f= Forca.new('oi')
+		f.chutar 'a'
+		f.chutes.should == 9
+	end
+	
+	it 'se acerta não deve subtrair um chute' do
+		f= Forca.new('Pera')
+		f.chutar 'e'
+		f.chutes.should == 10
+	
 	end
 end
